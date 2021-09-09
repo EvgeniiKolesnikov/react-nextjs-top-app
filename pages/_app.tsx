3d0a461abbf5fdb646ef7f7eeade05b9deae461a
Evgenii Kolesnikov <jekow@yandex.ru>
2021-09-09 23:54:40 +0300

    Add fonts and colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,12 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <title>Top App - сравнение курсов</title>
         <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap'
+          rel='stylesheet'
+        />
       </Head>
       <Component {...pageProps} />
     </>
