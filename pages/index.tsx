80754a61480e4919e47e2a4440da9b607772bec4
Evgenii Kolesnikov <jekow@yandex.ru>
2021-09-11 23:36:37 +0300

    Add P component and arrow in Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, P } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -7,8 +7,11 @@ export default function Home(): JSX.Element {
       <Htag tag='h1'>Example text h1</Htag>
       <Htag tag='h2'>Example text h2</Htag>
       <Htag tag='h3'>Example text h3</Htag>
-      <Button appearance='primary' className='sd'>Button primary</Button>
-      <Button appearance='ghost'>Button ghost</Button>
+      <Button appearance='primary' arrow='right'>Button primary</Button>
+      <Button appearance='ghost' arrow='down'>Button ghost</Button>
+      <P size='l'>Large p</P>
+      <P size='m'>Medium p</P>
+      <P size='s'>Small/default p</P>
     </>
   );
 }
