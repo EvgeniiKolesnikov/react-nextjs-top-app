491d47639bac49dc487b81adf8ec4beeee1fd996
Evgenii Kolesnikov <jekow@yandex.ru>
2021-09-18 23:26:05 +0300

    Product layout 1

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { ProductProps } from './Product.props';
 import styles from './Product.module.scss';
 import cn from 'classnames';
+import { Card } from '../Card/Card';
+import { Button, Rating, Tag } from '..';
 
 export const Product = ({
   product,
@@ -9,13 +11,47 @@ export const Product = ({
   ...props
 }: ProductProps): JSX.Element => {
   return (
-    <div
-      className={cn(styles.product, className, {
-
-      })}
-      {...props}
-    >
-      {product.title}
-    </div>
+    <Card className={styles.product}>
+      <div className={styles.logo}>
+        <img
+          src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+          alt={product.title}
+        />
+      </div>
+      <div className={styles.title}>{product.title}</div>
+      <div className={styles.price}>{product.price}</div>
+      <div className={styles.credit}>{product.credit}</div>
+      <div className={styles.rating}>
+        <Rating rating={product.reviewAvg ?? product.initialRating} />
+      </div>
+      <div className={styles.tags}>
+        {product.categories.map((c) => (
+          <Tag key={c} color='ghost'>
+            {c}
+          </Tag>
+        ))}
+      </div>
+      <div className={styles.priceTitle}>цена</div>
+      <div className={styles.creditTitle}>кредит</div>
+      <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+      <div className={styles.hr}><hr /></div> 
+      <div className={styles.description}>{product.description}</div>
+      <div className={styles.feature}>фичи</div>
+      <div className={styles.advBlock}>
+        <div className={styles.advantages}>
+          <div>Преимущества</div>
+          <div>{product.advantages}</div>
+        </div>
+        <div className={styles.disdvantages}>
+          <div>Недостатки</div>
+          <div>{product.disadvantages}</div>
+        </div>
+      </div>
+      <div className={styles.hr}><hr /></div> 
+      <div className={styles.actions}>
+        <Button appearance='primary'>Узнать подробнее</Button>
+        <Button appearance='ghost' arrow='right'>Читать отзывы</Button>
+      </div>
+    </Card>
   );
 };
